e45ff9803aa5faa89ab57e97f205d19460221183
Your Name <you@example.com>
2025-11-23 18:39:02 -0300

    authenticate-controller

---
+++ src/infra/http/controllers/authenticate-controller.ts
@@ -0,0 +1,51 @@
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
+import { z } from 'zod';
+import { ZodValidationPipe } from '../pipe/zod-validation-pipe';
+import { CredentialAlreadyExistError } from '@/core/errors/credential-already-exist-error';
+import { AuthenticateUserUseCase } from '@/domain/aplication/use-case/user/authenticate-use-case';
+
+const authenticateBodySchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+const bodyValidationPipe = new ZodValidationPipe(authenticateBodySchema);
+@Controller('/authenticate')
+export class AuthenticateController {
+  constructor(private authenticateUseCase: AuthenticateUserUseCase) {}
+  @Post()
+  @HttpCode(200)
+  async handle(@Body(bodyValidationPipe) body: AuthenticateBodySchema) {
+    const { email, password } = body;
+
+    const result = await this.authenticateUseCase.execute({
+      email,
+      password,
+    });
+
+    if (result.isLeft()) {
+      const error = result.value;
+      switch (error.constructor) {
+        case CredentialAlreadyExistError:
+          throw new ConflictException(error.message);
+        default:
+          throw new BadRequestException(error.message);
+      }
+    }
+
+    const { access_token } = result.value;
+
+    return {
+      access_token,
+    };
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,12 @@ import { CreateAccountController } from './controllers/create-account-controller
 import { DatabaseModule } from '../database/database.module';
 import { CreateUserUseCase } from '@/domain/aplication/use-case/user/create-user-use-case';
 import { CryptographyModule } from '../cryptography/cryptography.module';
+import { AuthenticateController } from './controllers/authenticate-controller';
+import { AuthenticateUserUseCase } from '@/domain/aplication/use-case/user/authenticate-use-case';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
-  controllers: [CreateAccountController],
-  providers: [CreateUserUseCase],
+  controllers: [CreateAccountController, AuthenticateController],
+  providers: [CreateUserUseCase, AuthenticateUserUseCase],
 })
 export class HttpModule {}
